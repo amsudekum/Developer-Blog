4c294de19cb02e1b2c748d81eaae273a11ead3b2
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-03-23 23:41:26 -0500

    working on comment routes

---
+++ Develop/controllers/api/commentroutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { Blog, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+ Comment.findall({})
+ .then(dbCommentData => res.json(dbCommentData))
+ .catch(err) => {
+    console.log(Err);
+    res.status(500).json(err)
+ };
+ }
